a144a1d575156406718c1c4020c016f7d5da18af
annin-tofu <yuok5770@gmail.com>
2021-06-03 18:09:47 +0900

    app built

--- src/components/Product.js
+++ src/components/Product.js
@@ -15,11 +15,11 @@ function Product({ id, title, price, description, category, image }) {
 
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
-      <p className="absolute to-2 right-2 text-xs italic text-gray-400">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
         {category}
       </p>
       <Image src={image} height={200} width={200} objectFit="contain" />
-      <h4 className="mx-3">{title}</h4>
+      <h4 className="my-3">{title}</h4>
       <div className="flex">
         {Array(rating)
           .fill()
@@ -27,13 +27,13 @@ function Product({ id, title, price, description, category, image }) {
             <StarIcon className="h-5 text-yellow-500" />
           ))}
       </div>
-      <p className="text-xs mt-2 my-2 line-clamp-2">(description)</p>)
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
       <div className="mb-5">
-        <Currency quantity={price} currency="USD" />
+        <Currency quantity={price} currency="JPY" />
       </div>
       {hasPrime && (
-        <div className="flex items-corner space-x-2 mt-5">
-          <img className="w-12" src="https;links.papareact.come/fdw" alt="" />
+        <div className="flex items-center space-x-2 -mt-5">
+          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
           <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
       )}
