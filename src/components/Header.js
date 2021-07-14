41364bce4266638fed13d527c733add6a4151053
annin-tofu <yuok5770@gmail.com>
2021-07-15 08:22:29 +0900

    logo changed and .env.local added

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ function Header() {
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
             onClick={() => router.push("/")}
-            src="https://links.papareact.com/f90"
+            src="https://res.cloudinary.com/dhyagpwyl/image/upload/v1626228999/llamazon_n4qjdo.png"
             width={150}
             height={40}
             objectFit="contain"
