2544bbea791dae96168a9ff2c19bfe86212adb46
annin-tofu <yuok5770@gmail.com>
2021-07-16 12:10:26 +0900

    bug fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -76,7 +76,7 @@ function Header() {
       </div>
 
       {/* Bottom nav*/}
-      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+      {/* <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
         <p className="link flex items-center">
           <MenuIcon className="h-6 mr-1" />
           All
@@ -90,7 +90,7 @@ function Header() {
         <p className="link hidden lg:inline-flex">Buy Again</p>
         <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
         <p className="link hidden lg:inline-flex">Health & Personal Care</p>
-      </div>
+      </div> */}
     </header>
   );
 }
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -48,9 +48,7 @@ function Checkout() {
 
           <div className="flex flex-col p-5 space-y-10 bg-white">
             <h1 className="text-3xl border-b pb-4">
-              {items.length === 0
-                ? "Your Amazon Basket is empty."
-                : "Shopping Basket"}
+              {items.length === 0 ? "Your Basket is empty." : "Shopping Basket"}
             </h1>
 
             {items.map((item, i) => (
