5d159e40dfb291bc3d0633fd295018567d806f05
annin-tofu <yuok5770@gmail.com>
2021-07-16 12:00:14 +0900

    prime logo removed

--- src/components/Product.js
+++ src/components/Product.js
@@ -53,7 +53,7 @@ function Product({ id, title, price, description, category, image }) {
       </div>
       {hasPrime && (
         <div className="flex items-center space-x-2 -mt-5">
-          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+          {/* <img className="w-12" src="https://links.papareact.com/fdw" alt="" /> */}
           <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
       )}
